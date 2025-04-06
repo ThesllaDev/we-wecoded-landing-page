46042ded732a57b749d5261ad35c57530e592003
ThesllaDev <theslladev@gmail.com>
2025-04-06 02:08:01 -0300

    🔨➕🔧 Chore: setup Prettier, Tailwind custom colors and fonts

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,14 +1,15 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Press_Start_2P, Fira_Code } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+const pressStart2P = Press_Start_2P({
+	weight: "400",
+	variable: "--font-press-start-2p",
+	subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const firaCode = Fira_Code({
+	variable: "--font-fira-code",
+	subsets: ["latin"],
 });
 
 export const metadata = {
@@ -20,7 +21,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${pressStart2P.variable} ${firaCode.variable}`}
       >
         {children}
       </body>
