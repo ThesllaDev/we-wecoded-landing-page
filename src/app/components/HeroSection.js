866c267ba64ee90d18f29d4bced22e09fea29016
ThesllaDev <theslladev@gmail.com>
2025-04-07 01:47:39 -0300

    💄 style: update HeroSection text color and html scroll

--- src/app/components/HeroSection.js
+++ src/app/components/HeroSection.js
@@ -8,7 +8,7 @@ export default function MenuNavBar() {
         <h1 className="text-purple absolute top-[30%] bg-white/50 p-4 font-[family-name:var(--font-press-start-2p)] text-3xl md:max-w-[30ch]">
           We Celebration of Gender Equity in Software Development
         </h1>
-        <p className="absolute top-[70%] bg-black/50 p-2 text-center font-[family-name:var(--font-fira-code)] text-2xl md:max-w-[60ch]">
+        <p className="absolute top-[70%] bg-black/50 p-2 text-center font-[family-name:var(--font-fira-code)] text-2xl text-white md:max-w-[60ch]">
           Join our annual community celebration and amplify the diverse voices
           shaping the future of technology.
         </p>
